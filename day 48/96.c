f9eafde84313a4a18556b5e250c426c23ad891e4
raiarnav-create <raiarnav059@gmail.com>
2025-12-01 19:00:02 +0530

    Add function to reverse words in a sentence
    Implement a function to reverse each word in a sentence.

---
+++ day 48/96.c
@@ -0,0 +1,30 @@
+//Q96: Reverse each word in a sentence without changing the word order.
+
+/*
+Sample Test Cases:
+Input 1:
+I love coding
+Output 1:
+I evol gnidoc
+
+*/
+
+#include <stdio.h>
+
+void reverse_word(char str[], int start, int end)
+{
+    char temp;
+    while (start < end)
+    {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+void reverse_each_word(char str[])
+{
+    int i
