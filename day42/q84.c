6d864d2fbd18c7c837a4b1d56aeba2c89c9e86df
raiarnav-create <raiarnav059@gmail.com>
2025-12-01 18:48:19 +0530

    Create q84.c

---
+++ day42/q84.c
@@ -0,0 +1,16 @@
+//Q84: Convert a lowercase string to uppercase without using built-in functions.
+
+/*
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+HELLO
+
+*/
+
+#include <stdio.h>
+
+void to_upper(char str[])
+{
+    int
