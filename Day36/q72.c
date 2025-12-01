2e1abb89c45dc5c3653932fd00cdcbaac3f9ae54
raiarnav-create <raiarnav059@gmail.com>
2025-12-01 12:38:03 +0530

    Add matrix operations for input, print, and sum
    Implement matrix input, print, and sum functions.

---
+++ Day36/q72.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void matrixi(int n, int l, int ar[n][l])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            printf("Enter Element of matrix at index %d, %d\n", (i + 1), (j + 1));
+            scanf("%d", &ar[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void matrixp(int n, int l, int ar[n][l])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            printf("%d ", ar[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void sum(int n, int l, int ar[n][l])
+{
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            s += ar[i][j];
+        }
+    }
+    printf("Sum of all elements in matrix is: %d\n", s);
+}
+
+int main()
+{
+    int n, l;
+    printf("Enter size of array:");
+    scanf("%d", &n);
+    scanf("%d", &l);
+    int ar[n][l];
+    matrixi(n, l, ar);
+    // matrixp(n, l, ar);
+    sum(n, l, ar);
+    return 0;
+}
